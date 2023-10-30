a0696e8b3dbd30d939d321b781afe9cecc2cd8ab
YiFei <932525787@qq.com>
2023-10-30 16:11:16 +0800

    add sh

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 datas="FBDB15K FBYG15K"
 rates="0.2 0.5 0.8"
 expn=$1
+if [ ! -d "results/${expn}" ]; then
+  mkdir results/${expn}
+fi
+if [ ! -d "results/${expn}/backup" ]; then
+  mkdir results/${expn}/backup
+fi
+cp *.py results/${expn}/backup/
 for data in $datas ; do
   for rate in $rates ; do
       echo "sbatch -o ${data}_${rate}.log run.slurm 4 5 0.001 ${data} ${rate}"
